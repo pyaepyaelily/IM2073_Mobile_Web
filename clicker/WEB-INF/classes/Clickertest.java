f0f2d1d0afc0c1ce8a5c81197efa09a17890837d
suga_tofu <pyaepyaelily@gmail.com>
2022-04-02 22:00:20 +0800

    added clicker folder

---
+++ clicker/WEB-INF/classes/Clickertest.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.sql.*;
+import jakarta.servlet.*; // Tomcat 10
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.util.*;
+
+@WebServlet("/query") // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class Clickertest extends HttpServlet {
+
+    // The doGet() runs once per HTTP GET request to this servlet.
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Set the MIME type for the response message
+        response.setContentType("text/html");
+        // Get a output writer to write the response message into the network socket
+        PrintWriter out = response.getWriter();
+
+        // Print an HTML page as the output of the query
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head><title>Query Response</title></head>");
+        out.println("<body>");
+
+        try (
+                // Step 1: Allocate a database 'Connection' object
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/clickerdb?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                        "myuser", "xxxx"); // For MySQL
+                // The format is: "jdbc:mysql://hostname:port/databaseName", "username",
+                // "password"
+
+                // Step 2: Allocate a 'Statement' object in the Connection
+                Statement stmt = conn.createStatement();) {
+            // Step 3: Execute a SQL SELECT query
+            String option = request.getParameter("option");
+            String sqlStr = "INSERT INTO responsetable (qnsID, response, studentID) VALUES ( 1,'" + option + "',1)";
+            out.println("<p>" + sqlStr + "</p>"); // for debugging
+            int count = 0;
+            count = stmt.executeUpdate(sqlStr);
+
+            out.println("<h3>Thank you for your query.</h3>");
+
+        } catch (Exception ex) {
+            out.println("<p>Error: " + ex.getMessage() + "</p>");
+            out.println("<p>Check Tomcat console for details.</p>");
+            ex.printStackTrace();
+        } // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK
+          // 7)
+
+        out.println("</body></html>");
+        out.close();
+    }
+}
