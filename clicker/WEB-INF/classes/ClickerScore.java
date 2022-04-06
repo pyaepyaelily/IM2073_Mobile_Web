b803637cded1c6593b869dcebde56e28b4f12cd0
nicorin <ychua045@e.ntu.edu.sg>
2022-04-07 01:55:29 +0800

    modified ui of clickerscore servlet

--- clicker/WEB-INF/classes/ClickerScore.java
+++ clicker/WEB-INF/classes/ClickerScore.java
@@ -23,6 +23,9 @@ public class ClickerScore extends HttpServlet {
         out.println("<head><title>Query Response</title></head>");
         out.println("<body>");
 
+        out.write("<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>");
+
+
         try (
                 // Step 1: Allocate a database 'Connection' object
                 Connection conn = DriverManager.getConnection(
@@ -34,6 +37,10 @@ public class ClickerScore extends HttpServlet {
                 // Step 2: Allocate a 'Statement' object in the Connection
                 Statement stmt = conn.createStatement();) {
             // Step 3: Execute a SQL SELECT query
+
+            out.println("<header class='w3-display-container w3-content w3-wide' style='max-width:1500px' id='home'>");
+            out.println("<img class='w3-image' class='w3-opacity' src='https://images.unsplash.com/photo-1464618663641-bbdd760ae84a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' alt='cloud' width='1500' height='800'>");
+
             String option = request.getParameter("option");
             String qnsID = request.getParameter("qnsID");
 
@@ -41,15 +48,16 @@ public class ClickerScore extends HttpServlet {
             boolean results = stmt.execute(sqlStr1);
             ResultSet rset = stmt.executeQuery(sqlStr1); // Send the query to the server
 
+            out.println("<div class='w3-display-middle w3-margin-top w3-center'>");
             if (!rset.isBeforeFirst()) {
                 out.println("<h3>Your score is 0</h3>");
             } else {
 
                 while (rset.next()) {
                     if (rset.getInt("score") == 0) {
-                        out.println("<h3>Your score is 0</h3>");
+                        out.println("<h3 class= 'w3-text-white'>Your score is 0</h3>");
                     } else {
-                        out.println("<h3>Good job! Your score is " + rset.getInt("score") + "</h3>");
+                        out.println("<h3 class= 'w3-text-white'>Good job! Your score is " + rset.getInt("score") + "</h3>");
                     }
 
                 }
@@ -62,6 +70,8 @@ public class ClickerScore extends HttpServlet {
             // out.println("<h3>Sorry you got it wrong. Please proceed to next
             // question.</h3>");
             // }
+            out.println("</div>");
+            out.println("</header>");
 
         } catch (Exception ex) {
             out.println("<p>Error: " + ex.getMessage() + "</p>");
